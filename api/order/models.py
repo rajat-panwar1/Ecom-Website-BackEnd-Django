d9661de65f41055b4c78aec3b8a9f4c4ae52dd05
hail4heisenberg <hail4heisenberg@gmail.com>
2020-08-25 21:46:50 +0530

    'hyzen-store-backend'

---
+++ api/order/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from api.user.models import CustomUser
+from api.product.models import Product
+
+class Order(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
+
+    product_names = models.CharField(max_length=500)
+    total_products = models.CharField(max_length=500, default=0)
+    transaction_id = models.CharField(max_length=50, default=0)
+    total_amount = models.CharField(max_length=50, default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
